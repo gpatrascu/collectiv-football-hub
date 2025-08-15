38aba9e9c3076f9a201471548ffd8ad9fbb91a4c
George Patrascu <george.patrascu@yahoo.com>
2025-08-15 18:53:10 +0000

    a

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { Component } from '@angular/core';
   template: `
     <div>Hello {{value}}</div>
     <button (click)="loginWithFacebook()">Login with Facebook</button>
+    <button (click)="loginWithGoogle()">Login with Google</button>
   `,
 })
 export class AppComponent {
@@ -12,5 +13,9 @@ export class AppComponent {
 
   loginWithFacebook() {
     window.location.href = '/.auth/login/facebook';
+  }
+
+  loginWithGoogle() {
+    window.location.href = '/.auth/login/google';
   }
 }
