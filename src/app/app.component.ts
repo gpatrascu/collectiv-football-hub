aed4b0efcfd8c4be7a75f7b64f90ef0306c8f470
George Patrascu <george.patrascu@yahoo.com>
2025-08-15 15:37:20 +0000

    ,

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,15 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<div>Hello {{value}}</div>`,
+  template: `
+    <div>Hello {{value}}</div>
+    <button (click)="loginWithFacebook()">Login with Facebook</button>
+  `,
 })
 export class AppComponent {
   value = 'World';
+
+  loginWithFacebook() {
+    window.location.href = '/.auth/login/facebook';
+  }
 }
