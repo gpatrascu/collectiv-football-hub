71e2f4fa122871cb3703c88277d5fc12d90a4a61
George Patrascu <g.patrascu@criteo.com>
2025-08-16 07:37:32 +0300

    Add test function to diagnose deployment issues

---
+++ api/test.cs
@@ -0,0 +1,18 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+
+public static class TestApi
+{
+    [FunctionName("test")]
+    public static async Task<IActionResult> Run(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
+        ILogger log)
+    {
+        log.LogInformation("Test API called");
+        return new OkObjectResult(new { message = "Test function is working", timestamp = System.DateTime.UtcNow });
+    }
+}
